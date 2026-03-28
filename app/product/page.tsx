477f6ffb2a3a578f257dd133fba3586f2dc55fde
ilham <ilhamcandram@gmail.com>
2026-03-28 12:48:19 +0700

    /product/page.tsx: change fetch into read file json for SSR

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,11 +1,13 @@
 
 import ProductCard from "@/components/ProductCard"
 import ProductContainer from "@/components/ProductContainer"
+import fs from "fs"
 
 export default async function Page(){
-    const requestProducts = await fetch("http://localhost:3000/api/product")
-    const productGroup = await requestProducts.json()
-    
+    // const requestProducts = await fetch("http://localhost:3000/api/product")
+    // const productGroup = await requestProducts.json()
+    const productGroup = JSON.parse(fs.readFileSync(`${process.cwd()}/public/products.json`, "utf-8"))
+
     return (
         <div className="pt-20">
             {productGroup.map((item: any) => (
